e9dfddaf6e13d195a9dcae32b41c4dd89ce3638f
hunterborwick <62222564+hunterborwick@users.noreply.github.com>
2020-03-18 19:38:37 -0400

    Done

---
+++ Keplers_Eq_Solver_Elliptical.py
@@ -0,0 +1,36 @@
+#Hunter Borwick
+#3/18/2020
+#Kepler's Equation Solver for Eccentric Anomaly (Elliptical Orbits)
+
+import math
+if __name__ == "__main__":
+    #User input for meal anomaly and eccentricity 
+    M_e = float(input('Enter Mean Anomaly: '))
+    e = float(input('Enter Eccentricity: '))
+    
+    #Iteration Tolerance
+    tol = 1E-8
+
+    #Initial Guess at Eccentric Anomaly
+    if M_e < math.pi:
+        E = M_e + (e / 2)
+    if M_e > math.pi:
+        E = M_e - (e / 2)
+    
+    #Initial Conditions
+    f = E - e*math.sin(E) - M_e
+    f_prime = 1 - e*math.cos(E)
+    ratio = f / f_prime
+
+    #Numerical interation for ratio compared to tolerance 
+    while abs(ratio) > tol:
+        f = E - e*math.sin(E) - M_e
+        f_prime = 1 - e*math.cos(E)
+        ratio = f / f_prime
+
+        if abs(ratio) > tol:
+            E = E - ratio
+        if abs(ratio) < tol:
+            break
+
+    print('Eccentric Anomaly is:', E)
